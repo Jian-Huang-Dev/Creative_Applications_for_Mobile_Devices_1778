112a484cf45376bdbcd713d0d32e0c710398ef47
Jian Huang <johnbodun@hotmail.com>
2016-01-23 21:04:31 -0500

    Solution for assignment 1

---
+++ Mobile_app_assign_1/app/src/main/java/assign1/mobileapp/jianhuang/mobile_app_assign_1/MainActivity.java
@@ -0,0 +1,90 @@
+package assign1.mobileapp.jianhuang.mobile_app_assign_1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textBox;
+    ToggleButton toggleButton;
+    ImageView image;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        textBox = (TextView) findViewById(R.id.textBox);
+
+        image = (ImageView) findViewById(R.id.imageView);
+
+        // toggle on and off for the doggy image display
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) image.setVisibility(View.VISIBLE);
+                else image.setVisibility(View.GONE);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    // increase 1 everytime perform a button click
+    public void incrementOne(View view) {
+        counter++;
+        textBox.setText("Clicked " + Integer.toString(counter) + " Times");
+    }
+
+    // reset page to its initial state
+    public void resetPage() {
+        counter = 0;
+        textBox.setText(R.string.initial_textbox_text);
+        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            // reset page
+            resetPage();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
